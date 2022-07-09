af8d0c30f51c6b8e95ccf343b528500c7fa2b5a7
yoonhero <yoonhero06@naver.com>
2022-07-09 21:20:26 +0900

    Training Pipeline

--- gradients_torch.py
+++ gradients_torch.py
@@ -11,27 +11,31 @@ import torch.nn as nn
 # f = w * x
 
 # f = 2 * x
-X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
-Y = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
+X = torch.tensor([[1], [2], [3], [4]], dtype=torch.float32)
+Y = torch.tensor([[2], [4], [6], [8]], dtype=torch.float32)
 
-w = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
+X_test = torch.tensor([5], dtype=torch.float32)
 
-# model prediction
-def forward(x):
-    return w * x
+n_samples, n_features = X.shape
+print(n_samples, n_features)
 
-print(f'Prediction before training: f(5) = {forward(5):.3f}')
+input_size = n_features
+output_size = n_features
+
+model = nn.Linear(input_size, output_size)
+
+print(f'Prediction before training: f(5) = {model(X_test).item():.3f}')
 
 # Training
 learning_rate = 0.01
 n_iters = 100
 
 loss = nn.MSELoss()
-optimizer = torch.optim.SGD([w], lr=learning_rate)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 for epoch in range(n_iters):
     # prediction = forward pass
-    y_pred = forward(X)
+    y_pred = model(X)
 
     # loss
     l = loss(Y, y_pred)
@@ -45,8 +49,9 @@ for epoch in range(n_iters):
     # zero gradients
     optimizer.zero_grad()
 
-    if epoch % 10 == 0:
-        print(f"epoch {epoch+1}: w = {w:.3f}, loss = {l:.8f}")
+    if epoch % 10  == 0:
+        [w, b] = model.parameters()
+        print(f"epoch {epoch+1}: w = {w[0][0].item():.3f}, loss = {l:.8f}")
 
     
-print(f"Prediction after training: f(5) = {forward(5):.3f}")
+print(f'Prediction after training: f(5) = {model(X_test).item():.3f}')
